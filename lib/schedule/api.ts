37d9ecbaf8e740cf1f035de08ea21e2df2e8a1a0
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-09 18:25:50 +0300

    chore: use NEXT_PUBLIC prefix for env variable

--- lib/schedule/api.ts
+++ lib/schedule/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export type Categories = {
   categories: CategoryInfo[];
--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   poweredByHeader: false,
   output: "standalone",
-  env: {
-    API_URL: process.env.API_URL,
-  },
 };
 
 module.exports = nextConfig;
