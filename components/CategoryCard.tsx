08c442bdf944ff22e5ad8d0222e2d6fdef61590f
Dan <dan@dminc.ru>
2023-06-25 12:41:28 +0300

    feat: add new design of category cards

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,26 +1,27 @@
 import Link from "next/link";
 
 export type CategoryCardProps = {
+  icon: (props: { className?: string; fill?: string }) => React.JSX.Element;
   slug: string;
   title: string;
   shortDescription: string;
 }
 
-export default function CategoryCard({
-  slug,
-  title,
-  shortDescription
-}: CategoryCardProps) {
-  const path = `/schedule/${slug}`
+export default function CategoryCard(props: CategoryCardProps) {
+  const path = `/schedule/${props.slug}`
 
   return (
     <Link
-      key={slug}
+      key={props.slug}
       href={path}
-      className="hover:bg-background flex flex-col justify-between items-center border-8 border-border px-4 py-2 my-2 rounded-3xl text-center w-full sm:w-fit"
+      className="hover:bg-background flex flex-col justify-between items-center border-4 border-border px-4 py-6 my-2 rounded-3xl text-center basis-80 sm:w-fit"
     >
-      <p className="sm:text-2xl font-semibold selected">{title}</p>
-      {shortDescription}
+      <props.icon
+        className="my-1"
+        fill={`#9747FF`}
+      />
+      <p className="sm:text-2xl py-1 font-semibold">{props.title}</p>
+      <p className="text-white/75">{props.shortDescription}</p>
     </Link>
   )
 }
