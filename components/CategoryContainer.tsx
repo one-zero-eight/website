aacc61d278af2756c19b1f03862973888620c2c0
Dan <dan@dminc.ru>
2023-06-25 12:41:28 +0300

    feat: add new design of category cards

--- components/CategoryContainer.tsx
+++ components/CategoryContainer.tsx
@@ -1,16 +1,25 @@
 import { getCategories } from "@/lib/schedule/api";
 import { notFound } from "next/navigation";
 import CategoryCard from "@/components/CategoryCard";
+import CoreCoursesIcon from "@/components/icons/CoreCoursesIcon";
+import ElectivesIcon from "@/components/icons/ElectivesIcon";
+import SportIcon from "@/components/icons/SportIcon";
 
 export default async function CategoryContainer() {
   const categories = await getCategories();
   if (categories === undefined) {
     notFound();
   }
+  const categoriesIcons = {
+    "core-courses": CoreCoursesIcon,
+    "electives": ElectivesIcon,
+    "sport": SportIcon
+  }
   return (
-    <div className="flex flex-row flex-wrap sm:flex-nowrap gap-x-4 mt-8">
+    <div className="flex flex-row flex-wrap justify-center sm:flex-nowrap w-full gap-x-28 mt-8">
       {categories.categories.map((v) => (
         <CategoryCard
+          icon={categoriesIcons[v.slug]}
           slug={v.slug}
           title={v.title}
           shortDescription={v.shortDescription}
