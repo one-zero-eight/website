fda86833e35f0c9072c34d3ab74da448369003e2
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-26 23:37:01 +0300

    chore: do not use 'async' components when not necessary

--- app/schedule/layout.tsx
+++ app/schedule/layout.tsx
@@ -4,10 +4,6 @@ export const metadata: Metadata = {
   title: "Schedule",
 };
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default async function Page() {
+export default function Page() {
   redirect("/");
 }
