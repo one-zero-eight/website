4f7f189a20a6c5cb734c5fb5e21922e7a6ebeb31
Dan <dan@dminc.ru>
2023-06-26 13:06:25 +0300

    feat: add new icon

---
+++ components/icons/DoubleArrowIcon.tsx
@@ -0,0 +1,15 @@
+function DoubleArrowIcon(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={36}
+      viewBox="0 0 48 48"
+      width={36}
+      {...props}
+    >
+      <path d="M12.7024 36.237L10.3154 33.85L20.2154 23.95L10.3154 14.05L12.7024 11.6631L24.9894 23.95L12.7024 36.237ZM25.46 36.237L23.073 33.85L32.973 23.95L23.073 14.05L25.46 11.6631L37.747 23.95L25.46 36.237Z" fill="white"/>
+    </svg>
+  );
+}
+
+export default DoubleArrowIcon;
