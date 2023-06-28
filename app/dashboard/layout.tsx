3bc1d314efadf1499637ac9f0d62159519120e1d
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-28 21:34:10 +0300

    chore: fix 'canonical' urls

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Dashboard",
+  alternates: { canonical: "/dashboard" },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
--- app/schedule/layout.tsx
+++ app/schedule/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Schedule",
+  alternates: { canonical: "/schedule" },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
