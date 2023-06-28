da50b1e720872cdbbfbcb87d9be8df788a3d87ea
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-28 20:39:29 +0300

    fix: do not hide user menu while refetching getMe

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -10,14 +10,14 @@ function UserMenu() {
 
   // !isClient - SSR output and during hydration.
   // isError - Error getting the user. Assume not authenticated.
-  // isLoading || !data - Request sent but no data yet.
-  if (!isClient || isError || isLoading || !data) {
+  // !data - Request sent but no data yet.
+  if (!isClient || isError || !data) {
     return <button onClick={signIn}>Sign in</button>;
   }
 
   return (
     <div className="flex flex-col items-center">
-      <p className="text-white/75 text-base mr-2">{data.name}</p>
+      <p className="text-white/75 text-base">{data.name}</p>
       <button onClick={signOut}>Sign out</button>
     </div>
   );
