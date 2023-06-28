3dca8917e6d9bca2d04890c9f696f88df614827c
Dan <dan@dminc.ru>
2023-06-28 22:55:44 +0300

    style: redesign the schedule element

--- components/ScheduleElement.tsx
+++ components/ScheduleElement.tsx
@@ -1,4 +1,5 @@
 import { Calendar, Schedule } from "@/lib/schedule/api";
+import DownloadIcon from "@/components/icons/DownloadIcon";
 
 export type ScheduleElementProps = {
   schedule: Schedule;
@@ -13,14 +14,19 @@ function ScheduleElement({
 }: ScheduleElementProps) {
   return (
     <button
-      className="hover:bg-background flex flex-row justify-between items-center sm:text-2xl font-semibold  border-8 border-border p-4 my-2 rounded-3xl min-w-fit"
+      className="bg-background flex flex-row justify-between items-center sm:text-2xl px-7 py-5 my-2 rounded-3xl w-80"
       onClick={onClick}
     >
-      <div className="flex flex-col gap-2">
-        <p className="whitespace-nowrap text-left">{calendar.name}</p>
+      <div className="flex flex-col gap-0.5">
+        <p className="whitespace-nowrap text-left text-2xl font-medium">
+          {calendar.name}
+        </p>
         {schedule.filters.map((v) =>
           Object.hasOwn(calendar, v.alias) ? (
-            <p className="text-md text-inactive text-left" key={v.alias}>
+            <p
+              className="text-xl text-inactive text-left font-medium"
+              key={v.alias}
+            >
               {calendar[v.alias]}
             </p>
           ) : undefined
@@ -30,7 +36,8 @@ function ScheduleElement({
       <div
         className={`selected select-none whitespace-nowrap mr-2 rounded-xl w-fit text-right`}
       >
-        Import
+        <DownloadIcon fill={`rgba(256, 256, 256, 0.75)`} />
+        {/*Import*/}
       </div>
     </button>
   );
