abc8ff5cfd61576e46cf668f94289c6bd53e1865
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-06-29 17:26:48 +0300

    chore: add types for GroupCard

--- components/GroupCard.tsx
+++ components/GroupCard.tsx
@@ -1,12 +1,15 @@
 import DownloadIcon from "@/components/icons/DownloadIcon";
 import FavoriteIcon from "@/components/icons/FavoriteIcon";
+import { UserXGroupView } from "@/lib/events";
 
-export function GroupCard(props: any) {
+export type GroupCardProps = {
+  v: UserXGroupView;
+  favorite: boolean;
+};
+
+export function GroupCard(props: GroupCardProps) {
   return (
-    <div
-      key={props.v.group.path}
-      className="bg-background hover:bg-hover_color flex flex-row justify-between items-center sm:text-2xl px-7 py-5 my-2 rounded-3xl min-w-fit min-h-fit"
-    >
+    <div className="bg-background hover:bg-hover_color flex flex-row justify-between items-center sm:text-2xl px-7 py-5 my-2 rounded-3xl min-w-fit min-h-fit">
       <p className="text-left text-xl font-medium w-56">{props.v.group.name}</p>
       <div
         className={`flex flex-row selected select-none whitespace-nowrap mr-2 rounded-xl w-fit text-right`}
