718a4c240a6efaef5beb0cc6837b784d53b5cc61
Dan <dan@dminc.ru>
2023-06-29 16:37:36 +0300

    fix: rollback decomposition

--- components/CategoriesDropdown.tsx
+++ components/CategoriesDropdown.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { Categories, getCategoryInfoByCategories } from "@/lib/schedule/api";
 import { useRouter } from "next/navigation";
+import { Popover, Transition } from "@headlessui/react";
 import React from "react";
-import { Dropdown } from "@/components/Dropdown";
-import DropdownButton from "@/components/DropdownButton";
+import DropListIcon from "@/components/icons/DropListIcon";
 
 function CategoriesDropdown({
   categories,
@@ -20,15 +20,42 @@ function CategoriesDropdown({
   };
 
   return (
-    <Dropdown optionInfo={(categoryInfo && categoryInfo.title) || ""}>
-      {categories.categories.map((v) => (
-        <DropdownButton
-          title={v.title}
-          key={v.slug}
-          func={(e) => setSelected((e.target as any).value as string)}
-        />
-      ))}
-    </Dropdown>
+    <Popover className="relative sm:text-2xl w-max rounded-3xl">
+      <Popover.Button className="w-full rounded-3xl">
+        <div className="rounded-3xl flex flex-row items-center bg-background py-4 px-5 rounded-3xl text-secondary_hover">
+          <p className="mr-4 whitespace-nowrap">
+            {(categoryInfo && categoryInfo.title) || ""}
+          </p>
+
+          <DropListIcon />
+        </div>
+      </Popover.Button>
+
+      <Transition
+        enter="transition duration-120 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-125 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Popover.Panel className="absolute z-10 rounded-3xl bg-background ">
+          <div className="flex flex-col justify-center inline-block items-center divide-y divide-border">
+            {categories.categories.map((v) => (
+              <Popover.Button
+                className="w-full rounded-3xl flex flex-row items-center bg-background py-4 px-5 rounded-3xl text-secondary_hover"
+                value={v.slug}
+                key={v.slug}
+                onClick={(e) => setSelected((e.target as any).value as string)}
+              >
+                {v.title}
+              </Popover.Button>
+            ))}
+          </div>
+        </Popover.Panel>
+      </Transition>
+      <Popover.Panel className="absolute z-10"></Popover.Panel>
+    </Popover>
   );
 }
 
--- components/FilterDropdown.tsx
+++ components/FilterDropdown.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { Schedule } from "@/lib/schedule/api";
 import React from "react";
-import { Dropdown } from "@/components/Dropdown";
-import DropdownButton from "@/components/DropdownButton";
+import { Popover, Transition } from "@headlessui/react";
+import DropListIcon from "@/components/icons/DropListIcon";
 
 function FilterDropdown({
   schedule,
@@ -23,19 +23,40 @@ function FilterDropdown({
     .title;
 
   return (
-    <Dropdown optionInfo={selected || filterTitle}>
-      <DropdownButton
-        title={"All"}
-        onClick={(e) => setSelected((e.target as any).value)}
-      />
-      {variants.map((v) => (
-        <DropdownButton
-          title={v}
-          key={v}
-          onClick={(e) => setSelected((e.target as any).value)}
-        />
-      ))}
-    </Dropdown>
+    <Popover className="relative sm:text-2xl w-max rounded-3xl">
+      <Popover.Button className="w-full rounded-3xl">
+        <div className="rounded-3xl flex flex-row items-center bg-background py-4 px-5 rounded-3xl text-secondary_hover">
+          <p className="mr-4 whitespace-nowrap">{selected || filterTitle}</p>
+
+          <DropListIcon />
+        </div>
+      </Popover.Button>
+
+      <Transition
+        enter="transition duration-120 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-125 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Popover.Panel className="absolute z-10 rounded-3xl bg-background ">
+          <div className="flex flex-col justify-center inline-block items-center divide-y divide-border">
+            {variants.map((v) => (
+              <Popover.Button
+                className="w-full rounded-3xl flex flex-row items-center bg-background py-4 px-5 rounded-3xl text-secondary_hover"
+                value={v}
+                key={v}
+                onClick={(e) => setSelected((e.target as any).value)}
+              >
+                {v}
+              </Popover.Button>
+            ))}
+          </div>
+        </Popover.Panel>
+      </Transition>
+      <Popover.Panel className="absolute z-10"></Popover.Panel>
+    </Popover>
   );
 }
 
