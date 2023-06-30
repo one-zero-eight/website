2cd2d7297ee69e64298aace17a14c50e4d3872da
Dan <dan@dminc.ru>
2023-06-30 12:29:32 +0300

    chore: make dropdown icon smaller

--- components/icons/DropListIcon.tsx
+++ components/icons/DropListIcon.tsx
@@ -1,8 +1,8 @@
 function DropListIcon(props: any) {
   return (
     <svg
-      width={48}
-      height={48}
+      width={40}
+      height={40}
       viewBox="0 -960 960 960"
       xmlns="http://www.w3.org/2000/svg"
       className="fill-secondary hover:fill-secondary_hover"
