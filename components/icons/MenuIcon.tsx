6805d22a96869fbe8a75c307a029cb9bbc380d36
Dan <dan@dminc.ru>
2023-07-03 11:19:54 +0300

    feat: add menu icon

---
+++ components/icons/MenuIcon.tsx
@@ -0,0 +1,13 @@
+export function MenuIcon({ color, width, height }: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={height}
+      viewBox="0 -960 960 960"
+      width={width}
+      className={color}
+    >
+      <path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+    </svg>
+  );
+}
