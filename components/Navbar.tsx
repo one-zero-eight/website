005a63d110e8254863825fbee7bf6fbcba5f46c2
Dan <dan@dminc.ru>
2023-07-03 15:48:57 +0300

    fix: navbar overlapping with sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import UserMenu from "@/components/UserMenu";
 
 export function Navbar({ children }: any) {
   return (
-    <nav className="hidden lg:flex justify-between z-10 opacity-[0.999] pb-0 bg-background_dark items-start">
+    <nav className="hidden lg:flex justify-between z-[2] opacity-[0.999] pb-0 items-start">
       <div className="mr-auto">{children}</div>
       <UserMenu isMobile={false} isSidebar={false} />
     </nav>
