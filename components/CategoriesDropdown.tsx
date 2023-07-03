68e56c1a66224b0a386b51305f67d0c49866082b
Dan <dan@dminc.ru>
2023-07-03 11:39:42 +0300

    fix: dropdown overlaying

--- components/CategoriesDropdown.tsx
+++ components/CategoriesDropdown.tsx
@@ -20,7 +20,7 @@ function CategoriesDropdown({
   };
 
   return (
-    <Popover className="relative text-xl w-max z-10 opacity-[0.999] rounded-full focus:outline-none">
+    <Popover className="relative text-xl w-max z-[1] opacity-[0.999] rounded-full focus:outline-none">
       <Popover.Button className="w-full rounded-full focus:outline-none">
         <div className="rounded-full flex flex-row items-center bg-background py-2 px-5 text-xl text-secondary_hover">
           <p className="mr-4 whitespace-nowrap">
