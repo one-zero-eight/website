cd1efea3dac0f74cbec0123ec1097cde92b78642
Dan <dan@dminc.ru>
2023-07-07 13:28:47 +0300

    chore: add proper Navbar props

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,17 @@
 import UserMenu from "@/components/UserMenu";
 
-export function Navbar({ children }: any) {
+type NavbarProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+export function Navbar({ children, className }: NavbarProps) {
   return (
-    <nav className="hidden lg:flex justify-between z-[2] opacity-[0.999] pb-0 items-start">
+    <nav
+      className={
+        "hidden lg:flex justify-between z-[2] opacity-[0.999] pb-0 items-start " +
+        className
+      }
+    >
       <div className="mr-auto">{children}</div>
       <UserMenu isMobile={false} isSidebar={false} />
     </nav>
