e28e7c75811ea5afc07ba9dfe01aff07a48224a5
Dan <dan@dminc.ru>
2023-07-07 15:06:51 +0300

    fix: add weight and height for CategoryCard icons

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
+import { IconProps } from "@/lib/types/IconProps";
 
 export type CategoryCardProps = {
-  icon: (props: { className?: string; fill?: string }) => React.JSX.Element;
+  icon: (props: IconProps) => React.JSX.Element;
   slug: string;
   title: string;
   shortDescription: string;
@@ -16,7 +17,7 @@ export default function CategoryCard(props: CategoryCardProps) {
       href={path}
       className="hover:bg-background shadow-5xl transition ease-in-out hover:shadow-5xl-m flex flex-col justify-between items-center border-4 border-border px-4 py-6 my-2 rounded-3xl text-center sm:w-80 lg:w-64 xl:basis-80 sm:w-fit"
     >
-      <props.icon className="my-1" fill={`#9747FF`} />
+      <props.icon className="my-1" width={48} height={48} fill={`#9747FF`} />
       <p className="text-xl xl:text-2xl py-1 font-semibold">{props.title}</p>
       <p className="text-base xl:text-lg text-white/75">
         {props.shortDescription}
