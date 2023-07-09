b61b3ba34618aceaffcc437fc84e6bd149007297
Dan <dan@dminc.ru>
2023-07-09 10:19:56 +0300

    feat: add account and exit icons

---
+++ components/icons/ExitIcon.tsx
@@ -0,0 +1,16 @@
+import { IconProps } from "@/lib/types/IconProps";
+
+export function ExitIcon({ width, height, className, fill }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height={height || 48}
+      viewBox="0 -960 960 960"
+      width={width || 48}
+      className={className}
+      fill={fill}
+    >
+      <path d="M179.761-114.022q-27.698 0-48.034-20.265-20.336-20.266-20.336-47.865v-595.696q0-27.697 20.336-48.033 20.336-20.337 48.034-20.337h292.674v68.37H179.761v595.696h292.674v68.13H179.761Zm486.478-182.369-48.978-48.5 101.043-101.044H371.891v-68.13h344.413L615.261-615.109l48.978-48.5L848.609-479l-182.37 182.609Z" />
+    </svg>
+  );
+}
