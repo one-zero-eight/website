30814f013313f67a04c1494280c454859e9731dc
Dan <dan@dminc.ru>
2023-07-15 18:56:45 +0300

    fix: size of category cards

--- components/CategoryCard.tsx
+++ components/CategoryCard.tsx
@@ -15,7 +15,7 @@ export default function CategoryCard(props: CategoryCardProps) {
     <Link
       key={props.slug}
       href={path}
-      className="text-text-main hover:bg-primary-main shadow-5xl transition ease-in-out hover:shadow-5xl-m flex flex-col justify-between items-center border-4 border-border px-4 py-6 my-2 rounded-3xl text-center sm:w-80 lg:w-64 xl:basis-80 sm:w-fit"
+      className="text-text-main hover:bg-primary-main shadow-5xl transition ease-in-out hover:shadow-5xl-m flex flex-col justify-between items-center border-4 border-border px-4 py-6 my-2 rounded-3xl text-center basis-80"
     >
       <props.icon className="my-1" width={48} height={48} fill={`#9747FF`} />
       <p className="text-xl xl:text-2xl py-1 font-semibold">{props.title}</p>
