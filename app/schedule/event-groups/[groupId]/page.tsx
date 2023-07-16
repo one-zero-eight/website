ccae39c2371fdc767561b0aa9034573337686cd4
Dan <dan@dminc.ru>
2023-07-16 12:27:29 +0300

    refactor: move the EventGroup page to a separate component

--- app/schedule/event-groups/[groupId]/page.tsx
+++ app/schedule/event-groups/[groupId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Navbar } from "@/components/Navbar";
 import { useEventGroupsGetEventGroup } from "@/lib/events";
 import React from "react";
+import { EventGroupPage } from "@/components/EventGroupPage";
 
 export type Props = {
   params: { groupId: string };
@@ -15,22 +15,5 @@ export default function Page({ params }: Props) {
     return <>Loading...</>;
   }
 
-  return (
-    <div className="p-16 items-center lg:[align-items:normal] flex flex-col">
-      <h1 className="text-text-main lgw-smh:hidden lgw-smh:invisible text-center xl:text-left text-3xl xl:text-4xl font-bold">
-        {data.name}
-      </h1>
-      <p className="lgw-smh:hidden lgw-smh:invisible text-center xl:text-left text-text-secondary/75">
-        {data.satellite?.description || ""}
-      </p>
-      <Navbar>
-        <h1 className="text-text-main text-center lg:text-left text-3xl lg:text-4xl font-bold">
-          {data.name}
-        </h1>
-        <p className="text-center xl:text-left text-text-secondary/75">
-          {data.satellite?.description || ""}
-        </p>
-      </Navbar>
-    </div>
-  );
+  return <EventGroupPage data={data} />;
 }
