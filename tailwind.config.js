fb3a9f126e79e1d4d34f084e4cf66557b061e3f7
Dan <dan@dminc.ru>
2023-07-17 00:25:03 +0300

    chore: add the new width

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      width: {
+        256: "64rem",
+      },
       colors: {
         base: "rgba(var(--color-base) / <alpha-value>)",
         primary: {
