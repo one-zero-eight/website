53dd1f4dd5502e8248e306a39250a12d865fb052
Dan <dan@dminc.ru>
2023-07-16 18:20:11 +0300

    fix: calendar display in popup

--- app/schedule/event-groups/[groupId]/page.tsx
+++ app/schedule/event-groups/[groupId]/page.tsx
@@ -2,6 +2,10 @@
 import { useEventGroupsGetEventGroup } from "@/lib/events";
 import React from "react";
 import { EventGroupPage } from "@/components/EventGroupPage";
+import { Navbar } from "@/components/Navbar";
+import { SCHEDULE_API_URL } from "@/lib/schedule/api";
+import Calendar from "@/components/Calendar";
+import { useWindowSize } from "usehooks-ts";
 
 export type Props = {
   params: { groupId: string };
@@ -10,10 +14,30 @@ export type Props = {
 export default function Page({ params }: Props) {
   const groupId = Number(params.groupId);
   const { data } = useEventGroupsGetEventGroup(groupId);
-
+  const { width } = useWindowSize();
   if (!data) {
     return <>Loading...</>;
   }
-
-  return <EventGroupPage groupData={data} />;
+  return (
+    <>
+      <div className="p-16 bg-secondary-main w-full h-64 items-center lg:[align-items:normal] flex flex-col">
+        <Navbar />
+      </div>
+      <EventGroupPage groupData={data} isPopup={false} />
+      <div className="px-2">
+        <Calendar
+          urls={data.path ? [`${SCHEDULE_API_URL}/${data.path}`] : []}
+          initialView={
+            width
+              ? width >= 1280
+                ? "dayGridMonth"
+                : width >= 1024
+                ? "timeGridWeek"
+                : "listMonth"
+              : "dayGridMonth"
+          }
+        />
+      </div>
+    </>
+  );
 }
