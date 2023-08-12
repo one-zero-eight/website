3ea5a9f78d7f9369e5b32f6b6504fe88d57b6a0b
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-08-13 00:51:41 +0300

    chore: redirect user to dashboard after signing in

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -8,7 +8,10 @@ export function useAuthPaths() {
 
   const loginEndpoint = `${EVENTS_API_URL}/auth/${process.env.NEXT_PUBLIC_AUTH_PROVIDER}/login`;
   const signInURL = new URL(loginEndpoint);
-  signInURL.searchParams.append("return_to", currentURL);
+  signInURL.searchParams.append(
+    "return_to",
+    isClient ? `${window.location.origin}/dashboard` : "/",
+  );
 
   const logoutEndpoint = `${EVENTS_API_URL}/auth/logout`;
   const signOutURL = new URL(logoutEndpoint);
