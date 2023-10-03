2f8afb0c74e153b7cde2a6b8ef1dc89c4cd29dcd
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-10-03 17:13:02 +0300

    chore: hide dark mode icon using css to prevent glitching

--- components/SwitchThemeButton.tsx
+++ components/SwitchThemeButton.tsx
@@ -2,25 +2,25 @@
 import { DarkModeIcon } from "@/components/icons/DarkModeIcon";
 import { LightModeIcon } from "@/components/icons/LightModeIcon";
 import React from "react";
-import { useDarkMode, useIsClient } from "usehooks-ts";
+import { useDarkMode } from "usehooks-ts";
 
 export default function SwitchThemeButton() {
-  const isClient = useIsClient();
-  const { isDarkMode, toggle } = useDarkMode();
+  const { toggle } = useDarkMode();
   return (
     <button
       className="flex h-18p w-18p flex-col items-center justify-center rounded-2xl bg-primary-main hover:bg-primary-hover"
       onClick={toggle}
     >
-      {isClient && !isDarkMode ? (
-        <LightModeIcon className="flex fill-[#F0B132]" width={36} height={36} />
-      ) : (
-        <DarkModeIcon
-          className="flex fill-focus_color"
-          width={36}
-          height={36}
-        />
-      )}
+      <LightModeIcon
+        className="flex fill-[#F0B132] dark:hidden"
+        width={36}
+        height={36}
+      />
+      <DarkModeIcon
+        className="hidden fill-focus_color dark:flex"
+        width={36}
+        height={36}
+      />
     </button>
   );
 }
