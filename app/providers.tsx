c737fcfee998ac32c499d14d11ee3f439f0da41e
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-10-08 12:36:49 +0300

    fix: force react-query to send requests

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            networkMode: "offlineFirst",
+          },
+        },
+      }),
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
