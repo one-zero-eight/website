69b083772dfc2e46fbb6ce744dba12be59bc439b
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-10-08 10:36:52 +0300

    chore: add inh api handler to service worker

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 // PWA support
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
@@ -6,6 +8,23 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   cacheStartUrl: true,
   dynamicStartUrl: true,
   dynamicStartUrlRedirect: "/dashboard",
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/api\.innohassle\.ru\/.*/i,
+        handler: "NetworkFirst",
+        method: "GET",
+        options: {
+          cacheName: "inh-api-cache",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+        },
+      },
+    ],
+  },
 });
 
 // Next.js config
