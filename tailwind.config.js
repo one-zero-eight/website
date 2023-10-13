8ee3e15fe3333d51995243a7d04baa028a1b42af
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2023-10-13 22:34:31 +0300

    chore: add iconify tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
+
 /** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
@@ -61,5 +63,8 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [],
+  plugins: [
+    // Iconify plugin
+    addDynamicIconSelectors(),
+  ],
 };
