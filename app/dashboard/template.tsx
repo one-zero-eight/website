346d504b36ed0ab7da98f7533362732f0d3ef759
Dan <dan@dminc.ru>
2024-02-17 09:45:17 +0300

    chore(dashboard): use container queries

--- app/dashboard/template.tsx
+++ app/dashboard/template.tsx
@@ -2,7 +2,7 @@ import { NavbarTemplate } from "@/components/layout/Navbar";
 
 export default function Template({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex flex-col p-4 lg:p-12">
+    <div className="@container/content flex flex-col p-4 @2xl/main:p-12">
       <NavbarTemplate
         title="Dashboard"
         description="Your cozy space on this planet."
