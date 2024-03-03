50a6093ba91eef18c0d38acfb1750fd9db56ab67
Dan <dan@dminc.ru>
2024-03-03 17:29:43 +0300

    chore: improve popup appearance

--- app/@modal/(.)schedule/event-groups/[alias]/page.tsx
+++ app/@modal/(.)schedule/event-groups/[alias]/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import Calendar from "@/components/common/calendar/Calendar";
-import ExportButton from "@/components/schedule/ExportButton";
-import FavoriteButton from "@/components/schedule/group-card/FavoriteButton";
 import {
   getICSLink,
   useEventGroupsFindEventGroupByAlias,
@@ -19,6 +16,9 @@ import {
 } from "@floating-ui/react";
 import { useRouter } from "next/navigation";
 import React from "react";
+import FavoriteButton from "@/components/schedule/group-card/FavoriteButton";
+import ExportButton from "@/components/schedule/ExportButton";
+import Calendar from "@/components/common/calendar/Calendar";
 
 export type Props = {
   params: { alias: string };
@@ -42,7 +42,10 @@ export default function Page({ params: { alias } }: Props) {
   // Role props for screen readers
   const role = useRole(context);
 
-  const { getFloatingProps } = useInteractions([dismiss, role]);
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    dismiss,
+    role,
+  ]);
 
   return (
     <>
@@ -57,12 +60,15 @@ export default function Page({ params: { alias } }: Props) {
                 ref={refs.setFloating}
                 style={transitionStyles}
                 {...getFloatingProps()}
-                className="flex p-4"
+                className="flex h-fit w-full flex-col p-4 @2xl/event:w-3/4 @5xl/event:w-1/2"
               >
-                <div className="h-fit max-w-3xl overflow-hidden rounded-2xl bg-primary-main @lg/event:w-[768px]">
+                <div className="overflow-hidden rounded-2xl bg-primary-main">
+                  {/* EventGroup banner */}
                   <div
-                    style={{ backgroundImage: "url(/background-pattern.svg)" }}
-                    className="flex h-64 w-full items-start justify-end bg-secondary-main bg-repeat"
+                    style={{
+                      backgroundImage: "url(/background-pattern.svg)",
+                    }}
+                    className="flex h-64 w-full items-start justify-end rounded-t-2xl bg-secondary-main bg-repeat"
                   >
                     <div className="mr-2 flex flex-row gap-4">
                       <button
@@ -79,45 +85,66 @@ export default function Page({ params: { alias } }: Props) {
                       </button>
                     </div>
                   </div>
-                  {group && (
-                    <div className="flex flex-col p-4 @lg/event:p-8">
-                      <div className="mb-4 flex items-start justify-between gap-4">
-                        <div className="min-h-full flex-grow">
-                          <h1 className="text-3xl font-semibold">
-                            {group.name}
-                          </h1>
-                          <p className="mt-2 whitespace-pre-wrap text-base text-text-secondary/75">
-                            {group.description ||
-                              "Hello world, this is a long description about my life and this elective."}
-                          </p>
+                  {/* Group information */}
+                  <div className="flex flex-col p-4 @2xl/event:p-8">
+                    {/* Group name and description */}
+                    {group && (
+                      <div className="mb-4 flex flex-col">
+                        <div className="flex items-start justify-between gap-4">
+                          <div className="min-h-full flex-grow">
+                            <h1 className="text-3xl font-semibold">
+                              {group.name}
+                            </h1>
+                            <p className="mt-2 whitespace-pre-wrap text-base text-text-secondary/75">
+                              {group.description ||
+                                "Hello world, this is a long description about my life and this elective."}
+                            </p>
+                          </div>
+                          <FavoriteButton groupId={group.id} />
                         </div>
-                        <FavoriteButton groupId={group.id} />
                       </div>
+                    )}
+                    {/* Tags information */}
+                    <div className="flex flex-col">
                       <h2 className="my-4 flex text-3xl font-medium">Tags</h2>
                       <div className="flex flex-wrap gap-2">
-                        {group.tags?.map((tag) => (
-                          <div
-                            key={tag.id}
-                            className="flex w-fit rounded-2xl bg-secondary-main px-4 py-2"
-                          >
-                            {tag.name}
-                          </div>
-                        ))}
+                        {group ? (
+                          group.tags?.map((tag) => (
+                            <div
+                              key={tag.id}
+                              className="flex w-fit rounded-2xl bg-secondary-main px-4 py-2"
+                            >
+                              {tag.name}
+                            </div>
+                          ))
+                        ) : (
+                          <p className="text-white/75">Loading tags...</p>
+                        )}
                       </div>
-                      <div className="my-4 flex flex-row flex-wrap items-center">
-                        <h2 className="flex grow text-3xl font-medium">
-                          Calendar
-                        </h2>
+                    </div>
+                    {/* Calendar title and Export button */}
+                    <div className="my-4 flex flex-row flex-wrap items-center">
+                      <h2 className="flex grow text-3xl font-medium">
+                        Calendar
+                      </h2>
+                      {group ? (
                         <ExportButton alias={group.alias} />
-                      </div>
-                      <div className="-mx-4 -mb-4 @lg/event:-mx-8 @lg/event:-mb-8">
+                      ) : (
+                        <p className="text-white/75">Loading...</p>
+                      )}
+                    </div>
+                    {/* Calendar itself */}
+                    <div className="-mx-4 -mb-4 rounded-b-2xl @2xl/event:-mx-8 @2xl/event:-mb-8">
+                      {group ? (
                         <Calendar
                           urls={[getICSLink(group.alias, user?.id)]}
                           viewId="popup"
                         />
-                      </div>
+                      ) : (
+                        <p className="text-white/75">Loading calendar...</p>
+                      )}
                     </div>
-                  )}
+                  </div>
                 </div>
               </div>
             </FloatingFocusManager>
