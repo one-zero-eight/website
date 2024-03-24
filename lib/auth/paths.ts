8d84d86172b5baf894840e2a453496ca1dee3d72
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-03-25 01:30:08 +0300

    fix: redirect_uri should be full

--- lib/auth/paths.ts
+++ lib/auth/paths.ts
@@ -1,20 +1,22 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useIsClient } from "usehooks-ts";
 
 export function useAuthPaths(signInRedirect?: string) {
   const isClient = useIsClient();
-  const currentURL = isClient ? window.location.href : "/";
+  const pathname = usePathname();
+  const origin = isClient ? window.location.origin : "";
 
   const loginEndpoint = `${process.env.NEXT_PUBLIC_ACCOUNTS_API_URL}/providers/${process.env.NEXT_PUBLIC_AUTH_PROVIDER}/login`;
   const signInURL = new URL(loginEndpoint);
   signInURL.searchParams.append(
     "redirect_uri",
-    signInRedirect ?? (isClient ? `${window.location.origin}/dashboard` : "/"),
+    origin + (signInRedirect || "/dashboard"),
   );
 
   const logoutEndpoint = `${process.env.NEXT_PUBLIC_ACCOUNTS_API_URL}/logout`;
   const signOutURL = new URL(logoutEndpoint);
-  signOutURL.searchParams.append("redirect_uri", currentURL);
+  signOutURL.searchParams.append("redirect_uri", origin + pathname);
 
   return {
     signIn: signInURL.toString(),
