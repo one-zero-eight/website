bfe1d537e83ad806d4d6f7c738c2de275593021a
alenaartemyeva <a.artemyeva@along.pw>
2024-06-16 21:00:02 +0300

    feat: change search result info

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -9,19 +9,14 @@ const SearchResult: React.FC<{
     <div className="m-4 grid auto-rows-max grid-cols-10 gap-4 @xl/content:grid-cols-2">
       <a
         href={file}
-        className="group col-span-3 flex flex-row gap-4 rounded-2xl bg-primary-main px-4 py-6 hover:bg-secondary-main"
+        className="group col-span-4 flex flex-col gap-2 rounded-2xl bg-primary-main px-4 py-6 hover:bg-secondary-main"
       >
-        <div className="w-12">
-          <span className="icon-[material-symbols--quick-reference-outline-rounded] text-5xl text-[#9747FF]" />
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-2xl font-semibold text-text-main">
-            Example Document
-          </p>
-          <p className="text-lg text-text-secondary/75">
-            This is a simple PDF file. Fun fun fun.
-          </p>
-        </div>
+        <p className="overflow-hidden text-ellipsis text-2xl font-semibold text-text-main">
+          {file}
+        </p>
+        <p className="text-lg text-text-secondary/75">
+          This is a simple PDF file. Fun fun fun.
+        </p>
       </a>
 
       <PdfPreview file={file} searchText={searchText}></PdfPreview>
