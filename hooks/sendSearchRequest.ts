a4e9f18ffacf20f0b03478425e6b5ad1ee64b16d
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-16 23:42:22 +0300

    chore: search by substring

--- hooks/sendSearchRequest.ts
+++ hooks/sendSearchRequest.ts
@@ -38,16 +38,24 @@ export async function findPdfFile(data: requestData): Promise<responseData> {
   const available_pdfs: responseData[] = [
     {
       file: "https://ontheline.trincoll.edu/images/bookdown/sample-local-pdf.pdf",
-      searchText: "sample-local-pdf.pdf",
+      searchText: "Example pdf",
     },
     {
-      file: "https://ontheline.trincoll.edu/images/bookdown/sample-local-pdf.pdf",
-      searchText: "example.pdf",
+      file: "https://digitaldesignschool.github.io/data/schoolRISCV/doc/riscv-spec-20191213.pdf",
+      searchText: "RISC-V instructions",
+    },
+    {
+      file: "https://arxiv.org/pdf/1706.03762",
+      searchText: "Transformers",
     },
   ];
 
   for (const pdf of available_pdfs) {
-    if (pdf.searchText === data.searchText) {
+    if (
+      pdf.searchText
+        .toLocaleLowerCase()
+        .includes(data.searchText.toLocaleLowerCase())
+    ) {
       return pdf;
     }
   }
