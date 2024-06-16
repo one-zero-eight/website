010ae9b8b172a13f9990a8f0d9dcb1d01395e1ad
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-06-16 23:51:30 +0300

    chore: open source in new tab

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -14,6 +14,7 @@ const SearchResult: React.FC<{
     <div className="m-4 grid auto-rows-max grid-cols-10 gap-4 @xl/content:grid-cols-2">
       <a
         href={file}
+        target="_blank"
         className="group col-span-4 flex flex-col gap-2 rounded-2xl bg-primary-main px-4 py-6 hover:bg-secondary-main"
       >
         <p className="overflow-hidden text-ellipsis text-2xl font-semibold text-text-main">
