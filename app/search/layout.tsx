90af70d98886912f1aa6589cc2a8c0a4d35eacbc
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-07-04 14:54:11 +0300

    fix: searchParams require Suspense boundary

--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Search",
@@ -7,5 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return <Suspense>{children}</Suspense>;
 }
