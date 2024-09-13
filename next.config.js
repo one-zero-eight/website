f9386cce2b11f1887198bfdd06ef936d66efe1c0
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-09-13 18:59:29 +0300

    chore: disable caching of api requests

--- next.config.js
+++ next.config.js
@@ -8,24 +8,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   cacheStartUrl: true,
   dynamicStartUrl: true,
   dynamicStartUrlRedirect: "/dashboard",
-  extendDefaultRuntimeCaching: true,
-  workboxOptions: {
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/api\.innohassle\.ru\/.*/i,
-        handler: "NetworkFirst",
-        method: "GET",
-        options: {
-          cacheName: "inh-api-cache",
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-          networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-        },
-      },
-    ],
-  },
 });
 
 // Next.js config
