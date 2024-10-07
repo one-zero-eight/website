82bf5129bec3397f1b4bccf37712e59593a6f502
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-10-07 11:33:36 +0300

    fix(room-booking): fix incorrect wrapper style on some browsers

--- src/components/room-booking/BookingTimeline.tsx
+++ src/components/room-booking/BookingTimeline.tsx
@@ -2,5 +2,13 @@ import { FunctionComponent } from "react";
 import { applyVueInReact } from "veaury";
 import RoomBookingViewVue from "./BookingTimeline.vue";
 
-const BookingTimeline = applyVueInReact(RoomBookingViewVue);
+const BookingTimeline = applyVueInReact(RoomBookingViewVue, {
+  vue: {
+    componentWrapAttrs: {
+      // Fix problem with incorrect 'all: unset' style on Firefox and Safari
+      style: {},
+      className: "[all:unset]",
+    },
+  },
+});
 export default BookingTimeline as FunctionComponent<any>;
