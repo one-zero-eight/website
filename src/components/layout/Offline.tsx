47495361f9d41dc333c5d45c8595d17ced37cb01
dorley174 <67094121+dorley174@users.noreply.github.com>
2024-10-20 17:03:58 +0300

    Update Offline.tsx

--- src/components/layout/Offline.tsx
+++ src/components/layout/Offline.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 const OfflineNotification: React.FC = () => {
     const [isOffline, setIsOffline] = useState(false);
     const [isClosed, setIsClosed] = useState(false);
 
-    // чек статус
     useEffect(() => {
         const handleOffline = () => {
             setIsOffline(true);
-         };
+        };
 
         const handleOnline = () => {
             setIsOffline(false);
-            setIsClosed(false); // соо если интернет восстановлен
+            setIsClosed(false); // Сообщение закрывается, если интернет восстановлен
         };
 
         window.addEventListener('offline', handleOffline);
@@ -24,15 +24,19 @@ const OfflineNotification: React.FC = () => {
         };
     }, []);
 
-    // если пользователь онлайн или скрыл уведомление крестиком, ничего не выводить
-    if (!isOffline || isClosed) return null;
+    // Если пользователь нажал на крестик или интернет есть — скрываем сообщение
+    if (isClosed) return null;
 
     return (
-        <div className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-red-600 text-white py-3 px-6 rounded-lg shadow-lg flex items-center justify-between z-50">
-            <p className="text-sm md:text-base">No Internet coonection!</p>
-            <button className="ml-4 text-white text-lg md:text-xl font-bold hover:text-gray-300" onClick={() => setIsClosed(true)}>
-                &times;
-            </button>
+        <div className={`fixed left-1/2 transform -translate-x-1/2 transition-all duration-500 ease-in-out ${isOffline ? 'top-5 opacity-100' : 'top-[-100px] opacity-0'}`}>
+            <div className="bg-red-600 text-white py-3 px-6 rounded-lg shadow-lg flex items-center justify-between z-50">
+                <p className="text-sm md:text-base">
+                    We have lost contact with you.<br /> Looks like your internet connection has lost!
+                </p>
+                <button className="ml-4 text-white text-lg md:text-xl font-bold hover:text-gray-300" onClick={() => setIsClosed(true)}>
+                    &times;
+                </button>
+            </div>
         </div>
     );
 };
