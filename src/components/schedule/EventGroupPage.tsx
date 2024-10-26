ac240fa2c5ae83a1b4cc8de6ae3b1c709bd94231
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-10-26 03:22:14 +0300

    fix: some content is unvisible on event group page

--- src/components/schedule/EventGroupPage.tsx
+++ src/components/schedule/EventGroupPage.tsx
@@ -17,73 +17,76 @@ export function EventGroupPage({ alias }: { alias: string }) {
   const { width } = useWindowSize();
   const [exportModalOpen, setExportModalOpen] = useState(false);
 
+  if (!group) {
+    return (
+      <div className="flex min-h-full flex-col overflow-y-auto @container/content">
+        <Topbar title="Group" />
+      </div>
+    );
+  }
+
   return (
-    <>
-      {group && (
-        <Helmet>
-          <title>{group.name} group — Schedule</title>
-          <meta name="description" content={group.description ?? undefined} />
-        </Helmet>
-      )}
+    <div className="flex min-h-full flex-col overflow-y-auto @container/content">
+      <Helmet>
+        <title>{group.name} group — Schedule</title>
+        <meta name="description" content={group.description ?? undefined} />
+      </Helmet>
+
       <Topbar title="Group" />
-      {group && (
-        <div className="flex min-h-full flex-col overflow-y-auto @container/content">
-          <div
-            style={{ backgroundImage: "url(/background-pattern.svg)" }}
-            className="min-h-64 bg-primary-main bg-repeat p-4"
-          />
-          <div className="p-4">
-            <div className="mb-4 flex items-start justify-between gap-4">
-              <div className="flex min-h-full flex-grow flex-col gap-2">
-                <h1 className="text-3xl font-semibold">{group.name}</h1>
-                <p className="whitespace-pre-wrap text-base text-text-secondary/75">
-                  {group.description ||
-                    "Hello world, this is a long description about my life and this elective."}
-                </p>
-              </div>
-              <FavoriteButton groupId={group.id} />
-            </div>
-            <h2 className="my-4 flex text-3xl font-medium">Tags</h2>
-            <div className="flex flex-wrap gap-2">
-              {group.tags?.map((tag) => (
-                <div
-                  key={tag.id}
-                  className="flex w-fit rounded-2xl bg-secondary-main px-4 py-2"
-                >
-                  {tag.name}
-                </div>
-              ))}
-            </div>
-            <div className="mt-4 flex flex-row flex-wrap items-center">
-              <h2 className="flex grow text-3xl font-medium">Calendar</h2>
-              <ExportButton onClick={() => setExportModalOpen(true)} />
-            </div>
+      <div
+        style={{ backgroundImage: "url(/background-pattern.svg)" }}
+        className="min-h-64 bg-primary-main bg-repeat p-4"
+      />
+      <div className="p-4">
+        <div className="mb-4 flex items-start justify-between gap-4">
+          <div className="flex min-h-full flex-grow flex-col gap-2">
+            <h1 className="text-3xl font-semibold">{group.name}</h1>
+            <p className="whitespace-pre-wrap text-base text-text-secondary/75">
+              {group.description ||
+                "Hello world, this is a long description about my life and this elective."}
+            </p>
           </div>
-          <Calendar
-            urls={[
-              {
-                url: getICSLink(group.alias, eventsUser?.id),
-                eventGroup: group,
-              },
-            ]}
-            initialView={
-              width
-                ? width >= 1280
-                  ? "dayGridMonth"
-                  : width >= 1024
-                    ? "timeGridWeek"
-                    : "listMonth"
-                : "dayGridMonth"
-            }
-            viewId="page"
-          />
+          <FavoriteButton groupId={group.id} />
         </div>
-      )}
+        <h2 className="my-4 flex text-3xl font-medium">Tags</h2>
+        <div className="flex flex-wrap gap-2">
+          {group.tags?.map((tag) => (
+            <div
+              key={tag.id}
+              className="flex w-fit rounded-2xl bg-secondary-main px-4 py-2"
+            >
+              {tag.name}
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 flex flex-row flex-wrap items-center">
+          <h2 className="flex grow text-3xl font-medium">Calendar</h2>
+          <ExportButton onClick={() => setExportModalOpen(true)} />
+        </div>
+      </div>
+      <Calendar
+        urls={[
+          {
+            url: getICSLink(group.alias, eventsUser?.id),
+            eventGroup: group,
+          },
+        ]}
+        initialView={
+          width
+            ? width >= 1280
+              ? "dayGridMonth"
+              : width >= 1024
+                ? "timeGridWeek"
+                : "listMonth"
+            : "dayGridMonth"
+        }
+        viewId="page"
+      />
       <EventGroupExportModal
         alias={group?.alias ?? ""}
         open={exportModalOpen}
         onOpenChange={setExportModalOpen}
       />
-    </>
+    </div>
   );
 }
