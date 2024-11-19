59e4e9b4ebaef21d1f7bd525da9cf2fca963b6bc
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-11-20 00:54:14 +0300

    chore: check pwa update right after reload

--- src/app/register-sw.ts
+++ src/app/register-sw.ts
@@ -8,25 +8,28 @@ export function registerServiceWorker() {
 
       // Check for updates periodically
       // https://vite-pwa-org.netlify.app/guide/periodic-sw-updates.html
-      setInterval(
-        async () => {
-          if (r.installing || !navigator) return;
+      async function checkUpdate() {
+        if (r === undefined || r.installing || !navigator) return;
 
-          // Do not try to update service worker if offline
-          if ("connection" in navigator && !navigator.onLine) return;
+        // Do not try to update service worker if offline
+        if ("connection" in navigator && !navigator.onLine) return;
 
-          const resp = await fetch(swUrl, {
+        const resp = await fetch(swUrl, {
+          cache: "no-store",
+          headers: {
             cache: "no-store",
-            headers: {
-              cache: "no-store",
-              "cache-control": "no-cache",
-            },
-          });
+            "cache-control": "no-cache",
+          },
+        });
 
-          if (resp?.status === 200) await r.update();
-        },
-        30 * 60 * 1000, // Every 30 minutes
-      );
+        if (resp?.status === 200) await r.update();
+      }
+
+      // Check after page load
+      checkUpdate();
+
+      // Check every 30 minutes
+      setInterval(checkUpdate, 30 * 60 * 1000);
     },
   });
 }
