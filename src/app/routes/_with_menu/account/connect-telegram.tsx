23898118b677837d50f92e8abee11d543894d636
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2024-11-19 06:18:09 +0300

    chore: rename 'icon' to 'favicon'; extend webmanifest

--- src/app/routes/_with_menu/account/connect-telegram.tsx
+++ src/app/routes/_with_menu/account/connect-telegram.tsx
@@ -19,7 +19,7 @@ export const Route = createFileRoute("/_with_menu/account/connect-telegram")({
 
       <div className="m-4 flex w-full max-w-md flex-col gap-4 rounded-2xl bg-primary-main px-4 py-6 @container/account">
         <img
-          src="/icon.svg"
+          src="/favicon.svg"
           alt="InNoHassle logo"
           className="h-24 w-24 self-center"
         />
