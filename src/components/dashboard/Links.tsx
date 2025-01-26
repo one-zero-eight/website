efd53a84a757696eda0b1944d062c6957cfcce78
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-26 11:30:48 +0300

    fix: improve link accessibility by using anchor tags instead of div

--- src/components/dashboard/Links.tsx
+++ src/components/dashboard/Links.tsx
@@ -1,18 +1,16 @@
 import universityResources from "@/lib/links/universityResources";
 
 const Links = () => {
-  const navigation = (link: string) => {
-    window.open(link, "_blank");
-  };
   return (
     <div className="px-4 py-8">
       <h3 className="my-8 text-3xl font-medium">All University Services</h3>
       <div className="grid gap-5 @4xl/content:grid-cols-2">
         {universityResources.map((resource, index) => (
-          <div
+          <a
+            href={resource.url}
+            target="_blank"
             key={index}
             className="flex h-[150px] cursor-pointer flex-row gap-6 rounded-2xl bg-primary px-4 py-6 transition-all ease-in-out hover:bg-primary-hover"
-            onClick={() => navigation(resource.url)}
           >
             <div className="w-10">
               <span
@@ -27,7 +25,7 @@ const Links = () => {
                 {resource.description && <p>{resource.description}</p>}
               </p>
             </div>
-          </div>
+          </a>
         ))}
       </div>
     </div>
