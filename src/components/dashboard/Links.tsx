b5cb21ec40911d2738a24237eb8301d2e21d5b6f
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2025-02-01 03:02:12 +0300

    chore: make links list more convenient to use

--- src/components/dashboard/Links.tsx
+++ src/components/dashboard/Links.tsx
@@ -13,12 +13,14 @@ const Links = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const fuse = useMemo(() => {
-    return new Fuse(universityResources, {
-      keys: ["title", "description"],
-      threshold: 0.3,
-    });
-  }, [universityResources]);
+  const fuse = useMemo(
+    () =>
+      new Fuse(universityResources, {
+        keys: ["title", "description"],
+        threshold: 0.3,
+      }),
+    [],
+  );
 
   const filteredResources = useMemo(() => {
     if (searchQuery) {
@@ -32,6 +34,7 @@ const Links = () => {
   useEffect(() => {
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
     setVisibleItems([]);
+    setActiveIndex(0);
 
     filteredResources.forEach((_, index) => {
       timeoutRef.current = setTimeout(() => {
@@ -49,10 +52,13 @@ const Links = () => {
       if (filteredResources.length === 0) return;
 
       if (event.key === "Enter") {
+        event.preventDefault();
         window.open(filteredResources[activeIndex].url, "_blank");
       } else if (event.key === "ArrowDown") {
+        event.preventDefault();
         setActiveIndex((prev) => (prev + 1) % filteredResources.length);
       } else if (event.key === "ArrowUp") {
+        event.preventDefault();
         setActiveIndex((prev) =>
           prev === 0 ? filteredResources.length - 1 : prev - 1,
         );
@@ -62,14 +68,9 @@ const Links = () => {
   );
 
   return (
-    <div
-      className="px-4 py-8"
-      onKeyDown={handleKeyDown}
-      tabIndex={0}
-      role="listbox"
-    >
+    <div className="px-4 py-8">
       <div className="flex flex-col gap-4 py-4 lg:flex-row">
-        <h3 className="my-8 px-2 text-2xl font-medium sm:my-2 sm:text-3xl lg:flex-[4] xxl:flex-none">
+        <h3 className="my-8 text-2xl font-medium sm:my-2 sm:text-3xl lg:flex-[4] xxl:flex-none">
           All University Services
         </h3>
         <div className="grid items-center gap-4 md:grid-cols-2 md:gap-6 lg:flex lg:flex-1 xxl:grid xxl:flex-auto xxl:grid-cols-2">
@@ -79,6 +80,7 @@ const Links = () => {
             onChange={setActiveGroup}
           />
           <SearchInput
+            onKeyDown={handleKeyDown}
             searchQuery={searchQuery}
             setSearchQuery={setSearchQuery}
           />
@@ -88,9 +90,7 @@ const Links = () => {
       <div className="flex min-h-[500px] flex-row-reverse items-start gap-6">
         {/* Category List */}
         <div className="hidden flex-1 lg:block xxl:hidden">
-          <h3 className="text-lg font-semibold text-contrast">
-            Group of Services
-          </h3>
+          <h3 className="text-lg font-semibold text-contrast">Filter</h3>
           <div>
             {groups.map(({ value }) => (
               <button
@@ -110,36 +110,41 @@ const Links = () => {
           </div>
         </div>
 
-        <div className="grid flex-[4] grid-cols-1 gap-5 lg:grid-cols-3 xxl:grid-cols-2">
-          {filteredResources.map((resource, index) => (
-            <a
-              href={resource.url}
-              target="_blank"
-              key={index}
-              className={clsx(
-                "resource-item flex min-h-[100px] cursor-pointer flex-row gap-4 rounded-2xl bg-primary px-4 py-4 transition-all ease-in-out hover:bg-primary-hover",
-                visibleItems.includes(index) ? "visible" : "",
-                activeIndex === index && searchQuery
-                  ? "border-2 border-brand-violet"
-                  : "",
-              )}
-            >
-              <div className="w-8">
-                <span
-                  className={`text-3xl ${resource.icon} text-brand-violet`}
-                />
-              </div>
-              <div>
-                <h3 className="text-lg font-semibold text-contrast">
-                  {resource.title}
-                </h3>
-                <p className="text-sm text-contrast/75">
-                  {resource.description}
-                </p>
-              </div>
-            </a>
-          ))}
-        </div>
+        {filteredResources.length > 0 ? (
+          <div className="grid flex-[4] grid-cols-1 gap-5 lg:grid-cols-3 xxl:grid-cols-2">
+            {filteredResources.map((resource, index) => (
+              <a
+                href={resource.url}
+                target="_blank"
+                rel="nofollow noreferrer"
+                key={index}
+                className={clsx(
+                  "resource-item flex min-h-[100px] flex-row gap-4 rounded-2xl bg-primary px-4 py-4 transition-all ease-in-out hover:bg-primary-hover",
+                  visibleItems.includes(index) ? "visible" : "",
+                  activeIndex === index && searchQuery
+                    ? "ring ring-brand-violet"
+                    : "",
+                )}
+              >
+                <div className="w-8">
+                  <span
+                    className={`text-3xl ${resource.icon} text-brand-violet`}
+                  />
+                </div>
+                <div>
+                  <h3 className="text-lg font-semibold text-contrast">
+                    {resource.title}
+                  </h3>
+                  <p className="text-sm text-contrast/75">
+                    {resource.description}
+                  </p>
+                </div>
+              </a>
+            ))}
+          </div>
+        ) : (
+          <p className="flex-[4] text-lg text-contrast">No results found.</p>
+        )}
       </div>
     </div>
   );
--- src/lib/links/SearchInput.tsx
+++ src/lib/links/SearchInput.tsx
@@ -2,17 +2,16 @@ interface SearchInputProps {
   searchQuery: string;
   setSearchQuery: (query: string) => void;
 }
-export const SearchInput: React.FC<SearchInputProps> = ({
-  searchQuery,
-  setSearchQuery,
-}) => {
+export const SearchInput: React.FC<
+  SearchInputProps & React.HTMLProps<HTMLDivElement>
+> = ({ searchQuery, setSearchQuery, ...props }) => {
   return (
-    <div className="relative flex w-full items-center gap-2">
+    <div className="relative flex w-full items-center gap-2" {...props}>
       <input
         autoComplete="off"
         spellCheck={false}
         className="inset-0 h-10 w-full resize-none rounded-lg border-2 border-brand-violet bg-pagebg p-3 text-base caret-brand-violet outline-none dark:text-white"
-        placeholder="Search anything"
+        placeholder="Search services..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         autoFocus={true}
@@ -21,9 +20,9 @@ export const SearchInput: React.FC<SearchInputProps> = ({
         <button
           type="button"
           onClick={() => setSearchQuery("")}
-          className="absolute right-2 top-1/2 -translate-y-1/2 transform"
+          className="absolute right-2 top-1/2 flex -translate-y-1/2 transform items-center"
         >
-          <span className="icon-[material-symbols--close] text-brand-violet" />
+          <span className="icon-[material-symbols--close] text-xl text-brand-violet" />
         </button>
       )}
     </div>
