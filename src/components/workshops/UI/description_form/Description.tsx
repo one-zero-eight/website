2f0a0f707c7c426827bbd8599e3ce8e956eee876
mao72 <kirillmao27@icloud.com>
2025-06-20 01:59:11 +0300

    Improve design of description modal and fix location of "inactive" component

--- src/components/workshops/UI/description_form/Description.tsx
+++ src/components/workshops/UI/description_form/Description.tsx
@@ -15,17 +15,43 @@ type Workshop = {
 interface WorkshopProps {
   workshop: Workshop | null;
 }
-
+const formatDate = (dateString: string) => {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  return date.toLocaleDateString("ru-RU");
+};
+const formatTime = (timeString: string) => {
+  if (!timeString) return "";
+  return timeString;
+};
 const Description: React.FC<WorkshopProps> = ({ workshop }) => {
   if (!workshop) return <div>No description</div>;
 
   return (
     <div className="description-content">
       <h2 className="description-title">{workshop.title}</h2>
-      <p>{workshop.body}</p>
-      <div className="description-body">
-        {workshop.date} {workshop.startTime} - {workshop.endTime} room:{" "}
-        {workshop.room}{" "}
+      <p style={{ marginBottom: "5px" }}>{workshop.body}</p>
+      <div className="flex flex-row items-center gap-2 text-xl text-contrast/75">
+        <div className="flex h-fit w-6">
+          <span className="icon-[material-symbols--location-on-outline] text-2xl" />
+        </div>
+        <p className="flex w-full items-center whitespace-pre-wrap py-1 [overflow-wrap:anywhere]">
+          {workshop.room}
+        </p>
+      </div>
+      <div className="flex flex-row items-center gap-2 text-xl text-contrast/75">
+        <div className="flex h-fit w-6">
+          <span className="icon-[material-symbols--today-outline] text-2xl" />
+        </div>
+        <p className="flex w-full items-center whitespace-pre-wrap py-1 [overflow-wrap:anywhere]">
+          {formatDate(workshop.date)}
+        </p>
+      </div>
+      <div className="flex flex-row items-center gap-2 text-xl text-contrast/75">
+        <div className="flex h-fit w-6">
+          <span className="icon-[material-symbols--schedule-outline] text-2xl" />
+        </div>
+        {formatTime(workshop.startTime) + "-" + formatTime(workshop.endTime)}
       </div>
     </div>
   );
