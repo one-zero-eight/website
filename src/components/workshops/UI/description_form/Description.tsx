0f488fca1e0eb8a1ace5a49d5e043ec030e96103
mao72 <kirillmao27@icloud.com>
2025-06-20 15:10:16 +0300

    Add clickability of links inside the description

--- src/components/workshops/UI/description_form/Description.tsx
+++ src/components/workshops/UI/description_form/Description.tsx
@@ -24,13 +24,52 @@ const formatTime = (timeString: string) => {
   if (!timeString) return "";
   return timeString;
 };
+const urlRegex = /https?:\/\/(.\.?)+\..+/;
+
+const ReplaceURL = (str: string) => {
+  const texts = str.split(" ").reduce(
+    (acc: { array: string[]; urls: string[]; text: string }, text) => {
+      if (urlRegex.test(text)) {
+        acc.array.push(acc.text);
+        acc.urls.push(text);
+        acc.text = " ";
+
+        return acc;
+      }
+
+      acc.text += `${text} `;
+
+      return acc;
+    },
+    { array: [], urls: [], text: "" },
+  );
+
+  if (texts.text) texts.array.push(texts.text);
+
+  const links = texts.urls.map((url) => {
+    return (
+      <a className="links" href={url} target="_blank" key={url}>
+        {url}
+      </a>
+    );
+  });
+
+  const merged = [];
+
+  for (let i = 0; i < texts.array.length; i += 1) {
+    merged.push(texts.array[i]);
+    if (links[i]) merged.push(links[i]);
+  }
+
+  return merged;
+};
 const Description: React.FC<WorkshopProps> = ({ workshop }) => {
   if (!workshop) return <div>No description</div>;
 
   return (
     <div className="description-content">
       <h2 className="description-title">{workshop.title}</h2>
-      <p style={{ marginBottom: "5px" }}>{workshop.body}</p>
+      <p style={{ marginBottom: "5px" }}>{ReplaceURL(workshop.body)}</p>
       <div className="flex flex-row items-center gap-2 text-xl text-contrast/75">
         <div className="flex h-fit w-6">
           <span className="icon-[material-symbols--location-on-outline] text-2xl" />
