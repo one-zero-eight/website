d2d7a1520740149329dbf3382359e02ed59b134c
mobgun <genya.moskal@gmail.com>
2025-07-10 14:33:46 +0300

    fix: minor lint fixes

--- src/components/workshops/UI/workshop_tiles/WorkshopItem.tsx
+++ src/components/workshops/UI/workshop_tiles/WorkshopItem.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { workshopsFetch } from "@/api/workshops";
 import { useNavigate } from "@tanstack/react-router";
