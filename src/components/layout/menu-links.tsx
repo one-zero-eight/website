e0b9109515fec12766d875348ecd05883dfd099f
dofi4ka <dofi4ka@yandex.ru>
2025-12-23 15:33:58 +0300

    chore: update sidebar links
    make guard public and add badge NEW
    remove NEW badges from search, clubs, timer

--- src/components/layout/menu-links.tsx
+++ src/components/layout/menu-links.tsx
@@ -32,11 +32,6 @@ export const items: ItemType[] = [
     title: "Search",
     to: "/search",
     icon: <span className="icon-[material-symbols--search]" />,
-    badge: (
-      <span className="bg-primary ml-2 rounded-full px-2 py-1 text-xs font-semibold text-white">
-        NEW
-      </span>
-    ),
   },
   { type: "separator" },
   {
@@ -88,11 +83,6 @@ export const items: ItemType[] = [
   {
     type: "local",
     title: "Clubs",
-    badge: (
-      <span className="bg-primary ml-2 rounded-full px-2 py-1 text-xs font-semibold text-white">
-        NEW
-      </span>
-    ),
     to: "/clubs",
     icon: <span className="icon-[material-symbols--diversity-1-rounded]" />,
   },
@@ -105,11 +95,6 @@ export const items: ItemType[] = [
   {
     type: "local",
     title: "Timer",
-    badge: (
-      <span className="bg-primary ml-2 rounded-full px-2 py-1 text-xs font-semibold text-white">
-        NEW
-      </span>
-    ),
     to: "/timer",
     icon: <span className="icon-[material-symbols--timer-outline-rounded]" />,
   },
@@ -152,21 +137,19 @@ export const items: ItemType[] = [
     to: "/extension",
     icon: <span className="icon-[material-symbols--extension-outline]" />,
   },
-  ...((import.meta.env.VITE_PRODUCTION && []) || [
-    {
-      type: "local",
-      title: "Guard",
-      badge: (
-        <span className="ml-2 rounded-full bg-gray-500 px-2 py-1 text-xs font-semibold text-white">
-          DEV
-        </span>
-      ),
-      to: "/guard",
-      icon: (
-        <span className="icon-[material-symbols--verified-user-outline-rounded]" />
-      ),
-    },
-  ]),
+  {
+    type: "local",
+    title: "Guard",
+    badge: (
+      <span className="bg-primary ml-2 rounded-full px-2 py-1 text-xs font-semibold text-white">
+        NEW
+      </span>
+    ),
+    to: "/guard",
+    icon: (
+      <span className="icon-[material-symbols--verified-user-outline-rounded]" />
+    ),
+  },
   {
     type: "local",
     title: "Forms",
