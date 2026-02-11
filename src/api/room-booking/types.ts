cfc54229e9ca268e159aefb3b475726f37558e06
dofi4ka <dofi4ka@yandex.ru>
2026-02-11 06:48:54 +0300

    feat!: update booking page for new api
    Instead of invalidatig queries after creating/deleting bookings use optimistic updates.
    Don't query myBookings just for rendering main timeline.
    Improve creating UX: add progress bar and show booking preview after creation.
    Show rooms' statuses and attendees' emails in bookings.

--- src/api/room-booking/types.ts
+++ src/api/room-booking/types.ts
@@ -100,18 +100,37 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
-  "/bookings/{booking_id}": {
+  "/bookings/{outlook_booking_id}": {
     parameters: {
       query?: never;
       header?: never;
       path?: never;
       cookie?: never;
     };
-    get?: never;
+    /** Get Booking */
+    get: operations["bookings_get_booking"];
     put?: never;
     post?: never;
     /** Delete Booking */
     delete: operations["bookings_delete_booking"];
+    options?: never;
+    head?: never;
+    /** Update Booking */
+    patch: operations["bookings_update_booking"];
+    trace?: never;
+  };
+  "/user/{user_id}/bookings": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    /** Get User Bookings */
+    get: operations["bookings_get_user_bookings"];
+    put?: never;
+    post?: never;
+    delete?: never;
     options?: never;
     head?: never;
     patch?: never;
@@ -121,8 +140,44 @@ export interface paths {
 export type webhooks = Record<string, never>;
 export interface components {
   schemas: {
+    /** Attendee */
+    Attendee: {
+      /** Email */
+      email: string;
+      status: components["schemas"]["BookingStatus"] | null;
+      /** Assosiated Room Id */
+      assosiated_room_id: string | null;
+    };
     /** Booking */
     Booking: {
+      /** Room Id */
+      room_id: string;
+      /**
+       * Start
+       * Format: date-time
+       */
+      start: string;
+      /**
+       * End
+       * Format: date-time
+       */
+      end: string;
+      /** Title */
+      title: string;
+      /** Outlook Booking Id */
+      outlook_booking_id: string | null;
+      /** Attendees */
+      attendees: components["schemas"]["Attendee"][] | null;
+      /**
+       * Id
+       * @description ID of the booking, computed from room_id, start and end
+       */
+      readonly id: string;
+    };
+    /** @enum {string} */
+    BookingStatus: BookingStatus;
+    /** CreateBookingRequest */
+    CreateBookingRequest: {
       /** Room Id */
       room_id: string;
       /** Title */
@@ -137,30 +192,22 @@ export interface components {
        * Format: date-time
        */
       end: string;
+      /** Participant Emails */
+      participant_emails: string[] | null;
     };
     /** HTTPValidationError */
     HTTPValidationError: {
       /** Detail */
       detail?: components["schemas"]["ValidationError"][];
     };
-    /** MyUniBooking */
-    MyUniBooking: {
-      /** Id */
-      id: number;
-      /** Room Id */
-      room_id: string;
+    /** PatchBookingRequest */
+    PatchBookingRequest: {
       /** Title */
-      title: string;
-      /**
-       * Start
-       * Format: date-time
-       */
-      start: string;
-      /**
-       * End
-       * Format: date-time
-       */
-      end: string;
+      title: string | null;
+      /** Start */
+      start: string | null;
+      /** End */
+      end: string | null;
     };
     /**
      * Room
@@ -220,10 +267,14 @@ export interface components {
   headers: never;
   pathItems: never;
 }
+export type SchemaAttendee = components["schemas"]["Attendee"];
 export type SchemaBooking = components["schemas"]["Booking"];
+export type SchemaCreateBookingRequest =
+  components["schemas"]["CreateBookingRequest"];
 export type SchemaHttpValidationError =
   components["schemas"]["HTTPValidationError"];
-export type SchemaMyUniBooking = components["schemas"]["MyUniBooking"];
+export type SchemaPatchBookingRequest =
+  components["schemas"]["PatchBookingRequest"];
 export type SchemaRoom = components["schemas"]["Room"];
 export type SchemaValidationError = components["schemas"]["ValidationError"];
 export type $defs = Record<string, never>;
@@ -365,6 +416,20 @@ export interface operations {
           "application/json": components["schemas"]["Booking"][];
         };
       };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Room not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
       /** @description Validation Error */
       422: {
         headers: {
@@ -378,17 +443,16 @@ export interface operations {
   };
   bookings_create_booking: {
     parameters: {
-      query: {
-        room_id: string;
-        title: string;
-        start: string;
-        end: string;
-      };
+      query?: never;
       header?: never;
       path?: never;
       cookie?: never;
     };
-    requestBody?: never;
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["CreateBookingRequest"];
+      };
+    };
     responses: {
       /** @description Successful Response */
       200: {
@@ -396,8 +460,22 @@ export interface operations {
           [name: string]: unknown;
         };
         content: {
-          "application/json": boolean;
+          "application/json": components["schemas"]["Booking"];
         };
+      };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
       };
       /** @description Validation Error */
       422: {
@@ -412,7 +490,10 @@ export interface operations {
   };
   bookings_my_bookings: {
     parameters: {
-      query?: never;
+      query?: {
+        start?: string | null;
+        end?: string | null;
+      };
       header?: never;
       path?: never;
       cookie?: never;
@@ -425,30 +506,22 @@ export interface operations {
           [name: string]: unknown;
         };
         content: {
-          "application/json": components["schemas"]["MyUniBooking"][];
+          "application/json": components["schemas"]["Booking"][];
         };
       };
-    };
-  };
-  bookings_delete_booking: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path: {
-        booking_id: number;
-      };
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description Successful Response */
-      200: {
+      /** @description Unauthorized */
+      403: {
         headers: {
           [name: string]: unknown;
         };
-        content: {
-          "application/json": boolean;
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
         };
+        content?: never;
       };
       /** @description Validation Error */
       422: {
@@ -461,6 +534,199 @@ export interface operations {
       };
     };
   };
+  bookings_get_booking: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        outlook_booking_id: string;
+      };
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      /** @description Successful Response */
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["Booking"];
+        };
+      };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Validation Error */
+      422: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["HTTPValidationError"];
+        };
+      };
+    };
+  };
+  bookings_delete_booking: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        outlook_booking_id: string;
+      };
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      /** @description Deleted successfully */
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": unknown;
+        };
+      };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Validation Error */
+      422: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["HTTPValidationError"];
+        };
+      };
+    };
+  };
+  bookings_update_booking: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path: {
+        outlook_booking_id: string;
+      };
+      cookie?: never;
+    };
+    requestBody: {
+      content: {
+        "application/json": components["schemas"]["PatchBookingRequest"];
+      };
+    };
+    responses: {
+      /** @description Successful Response */
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": unknown;
+        };
+      };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Validation Error */
+      422: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["HTTPValidationError"];
+        };
+      };
+    };
+  };
+  bookings_get_user_bookings: {
+    parameters: {
+      query: {
+        start: string;
+        end: string;
+      };
+      header?: never;
+      path: {
+        user_id: string;
+      };
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      /** @description Successful Response */
+      200: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["Booking"][];
+        };
+      };
+      /** @description Unauthorized */
+      403: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Booking not found */
+      404: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content?: never;
+      };
+      /** @description Validation Error */
+      422: {
+        headers: {
+          [name: string]: unknown;
+        };
+        content: {
+          "application/json": components["schemas"]["HTTPValidationError"];
+        };
+      };
+    };
+  };
+}
+export enum BookingStatus {
+  Accept = "Accept",
+  Tentative = "Tentative",
+  Decline = "Decline",
+  Unknown = "Unknown",
 }
 export enum RoomAccess_level {
   yellow = "yellow",
