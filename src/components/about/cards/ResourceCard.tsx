783d95434697042c15ee613eb664dd7c88ab5ddc
projacktor <1234xl.com@gmail.com>
2026-02-14 00:06:39 +0300

    fix: animation

--- src/components/about/cards/ResourceCard.tsx
+++ src/components/about/cards/ResourceCard.tsx
@@ -18,13 +18,11 @@ export function ResourceCard({
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className={`group animate-in zoom-in-50 relative h-[130px] w-[130px] transition-transform duration-500 hover:scale-105 ${className}`}
+      className={`group animate-in zoom-in-50 relative h-[130px] w-[130px] ${className}`}
     >
-      <div className="dark:bg-base-200 dark:border-base-100 group-hover:border-primary dark:group-hover:border-primary absolute top-0 left-0 flex min-h-full w-full flex-col items-center justify-center rounded-lg border-2 border-gray-200 bg-white py-3 shadow-sm transition-all duration-300 ease-in-out group-hover:shadow-xl dark:border">
+      <div className="dark:bg-base-200 dark:border-base-100 group-hover:border-primary dark:group-hover:border-primary absolute top-0 left-0 flex min-h-full w-full flex-col items-center justify-center rounded-lg border-2 border-gray-200 bg-white py-3 shadow-sm transition-all duration-300 group-hover:shadow-xl hover:scale-105 dark:border">
         <div className="flex w-full flex-col items-center justify-center gap-2">
-          <span
-            className={`${icon} text-4xl ${color} transition-transform duration-300 group-hover:scale-110`}
-          />
+          <span className={`${icon} text-4xl ${color}`} />
           <h3 className="px-2 text-center font-medium text-gray-900 dark:text-gray-100">
             {title}
           </h3>
