b9023bbfb58f9ed2c3260a22eee10b2d0a6b19ef
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2026-02-14 18:11:25 +0300

    chore(dashboard): show Telegram alias

--- src/components/dashboard/AccountWidget.tsx
+++ src/components/dashboard/AccountWidget.tsx
@@ -1,4 +1,5 @@
 import { useMe } from "@/api/accounts/user.ts";
+import { Link } from "@tanstack/react-router";
 
 export function AccountWidget() {
   const { me } = useMe();
@@ -6,8 +7,9 @@ export function AccountWidget() {
   if (!me) return null;
 
   const roles = [
-    me.innopolis_sso?.is_student && "Student",
-    me.innopolis_sso?.is_staff && "Staff",
+    me.innopolis_info?.is_staff && "Staff",
+    me.innopolis_info?.is_student && "Student",
+    me.innopolis_info?.is_college && "College",
   ]
     .filter(Boolean)
     .join(", ");
@@ -15,9 +17,9 @@ export function AccountWidget() {
   return (
     <div className="flex max-w-full flex-row gap-4">
       <div className="bg-base-200 text-base-content/50 flex h-20 w-20 shrink-0 items-center justify-center overflow-clip rounded-full">
-        {me.telegram?.photo_url ? (
+        {me.telegram_info?.photo_url ? (
           <img
-            src={me.telegram.photo_url}
+            src={me.telegram_info.photo_url}
             alt="Your avatar"
             className="border-base-content/50 rounded-full border-2"
           />
@@ -26,10 +28,34 @@ export function AccountWidget() {
         )}
       </div>
       <div className="flex flex-col justify-center overflow-x-hidden">
-        <p className="text-xl wrap-break-word">{me.innopolis_sso?.name}</p>
+        <p className="text-xl wrap-break-word">{me.innopolis_info?.name}</p>
         <p className="text-base-content/75 overflow-hidden text-sm text-ellipsis">
-          {me.innopolis_sso?.email}
+          {me.innopolis_info?.email}
           <span className="text-sm">{roles && ` (${roles})`}</span>
+        </p>
+        <p className="text-base-content/75 overflow-hidden text-sm text-ellipsis">
+          {me.telegram_info ? (
+            <>
+              @{me.telegram_info?.username || "no Telegram alias"}
+              {me?.telegram_update_data &&
+                !me?.telegram_update_data.success && (
+                  <Link
+                    to="/account/connect-telegram"
+                    search={{ reconnect: true }}
+                    className="link text-primary ml-1 text-sm"
+                  >
+                    Reconnect Telegram
+                  </Link>
+                )}
+            </>
+          ) : (
+            <Link
+              to="/account/connect-telegram"
+              className="link text-primary text-sm"
+            >
+              Connect Telegram
+            </Link>
+          )}
         </p>
       </div>
     </div>
