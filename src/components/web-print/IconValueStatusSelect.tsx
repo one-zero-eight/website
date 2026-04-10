a88b44d14af28111e796fe38d3d8d8862958c40c
danil <danillatency@gmail.com>
2026-04-11 02:36:25 +0300

    feat: custom integer input, central job configuration flow
    Relates one-zero-eight/printers#57

--- src/components/web-print/IconValueStatusSelect.tsx
+++ src/components/web-print/IconValueStatusSelect.tsx
@@ -1,22 +1,41 @@
 import selectStyles from "@/components/web-print/printers.select.module.css";
 import styles from "@/components/web-print/printers.module.css";
+import { useEffect, useRef } from "react";
 
 export function IconValueStatusSelect({
-  id,
   icons,
   names,
   values,
   statuses,
+  onSelected,
 }: {
-  id: string;
   icons: string[] | undefined;
   names: string[] | undefined;
   values: string[] | undefined;
   statuses: string[] | undefined;
+  onSelected: (value: string) => void;
 }) {
+  const inputReference = useRef<HTMLSelectElement | null>(null);
+
+  useEffect(() => {
+    const input = inputReference.current;
+    if (!input) return;
+
+    function pass(event: Event) {
+      onSelected((event.target as HTMLInputElement).value);
+    }
+
+    input.addEventListener("change", pass);
+    onSelected(input.value);
+
+    return () => {
+      if (input) input.removeEventListener("change", pass);
+    };
+  }, [onSelected, values]);
+
   return (
     <div className={selectStyles.selectWrapper}>
-      <select id={id} className={selectStyles.select}>
+      <select ref={inputReference} className={selectStyles.select}>
         {names?.map((printerDisplayName, i) => {
           if (!values) return <></>;
           else
