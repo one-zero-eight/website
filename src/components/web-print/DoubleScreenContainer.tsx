21d6ac6a7382f92a00e3f8386a7c99d3bacfb730
danil <danillatency@gmail.com>
2026-04-30 23:34:35 +0300

    feat: print job starting
    Also:
    - style fixes
    - use enums from auto generated API types
    
    Relates one-zero-eight/printers#57

--- src/components/web-print/DoubleScreenContainer.tsx
+++ src/components/web-print/DoubleScreenContainer.tsx
@@ -1,5 +1,15 @@
 import styles from "@/components/web-print/printers.module.css";
 
-export function DoubleScreenContainer({ children }: { children: any }) {
-  return <div className={styles.doubleScreenContainer}>{children}</div>;
+export function DoubleScreenContainer({
+  className,
+  children,
+}: {
+  className: string | boolean;
+  children: any;
+}) {
+  return (
+    <div className={`${styles.doubleScreenContainer} ${className}`}>
+      {children}
+    </div>
+  );
 }
