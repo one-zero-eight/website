1285d8507749b8e052abdd7c6fb92ce54ea1b94c
danil <danillatency@gmail.com>
2026-05-03 01:54:30 +0300

    feat: universal device choice
    Relates one-zero-eight/printers#57

---
+++ src/components/web-print/DeviceChoicePoint.tsx
@@ -0,0 +1,46 @@
+import { $printers } from "@/api/printers";
+import styles from "@/components/web-print/printers.module.css";
+import fontStyles from "@/components/web-print/printers.fonts.module.css";
+import { IconValueStatusSelect } from "@/components/web-print/IconValueStatusSelect.tsx";
+
+export function DeviceChoicePoint({
+  setDeviceName,
+  configurationType, // false for scanning
+}: {
+  setDeviceName: (value: string) => void;
+  configurationType: boolean;
+}) {
+  const { data: rawDevices } = configurationType
+    ? $printers.useQuery("get", "/print/get_printers")
+    : $printers.useQuery("get", "/scan/get_scanners");
+  const { data: rawStatuses } = configurationType
+    ? $printers.useQuery("get", "/print/get_printers_status")
+    : $printers.useQuery("get", "/scan/debug/get_scanners_status");
+
+  return (
+    <div className={styles.scrollPart__elem}>
+      <p className={fontStyles.formPointFont}>
+        {configurationType ? "Printer" : "Scanner"}
+      </p>
+      <IconValueStatusSelect
+        icons={rawDevices?.map(() => (configurationType ? "🖨️" : "📰"))}
+        names={rawDevices?.map((device) => device.display_name)}
+        values={rawDevices?.map((device) =>
+          // @ts-expect-error - dynamic object changing
+          configurationType ? device.cups_name : device.name,
+        )}
+        statuses={rawStatuses?.map((status) =>
+          status.offline
+            ? ", 💀 offline"
+            : configurationType
+              ? // @ts-expect-error - dynamic object changing
+                status.paper_percentage
+                ? ", 📃\xa0has\xa0paper"
+                : ", ✂️\xa0no\xa0paper"
+              : ", ✔️ online",
+        )}
+        onSelected={setDeviceName}
+      />
+    </div>
+  );
+}
