630fcfdd2eb8e8aef5d6dcae5c2b1514bab839b3
danillatency <danillatency@gmail.com>
2026-06-03 16:52:10 +0300

    feat: preliminary mode choice

---
+++ src/components/web-print/ConfigurationSelectionScreen.tsx
@@ -0,0 +1,41 @@
+import styles from "@/components/web-print/printers.module.css";
+import fontStyles from "@/components/web-print/printers.fonts.module.css";
+
+export function ConfigurationSelectionScreen({
+  setConfigurationType,
+}: {
+  setConfigurationType: (value: boolean) => void;
+}) {
+  return (
+    <div className={styles.ordinaryScreen}>
+      <div className={styles.ordinaryScreen__contentSizeRestrictor}>
+        <button
+          className={styles.fiftyButton}
+          onClick={() => {
+            setConfigurationType(false);
+          }}
+        >
+          <span
+            className={`icon-[material-symbols--adf-scanner-rounded] ${styles.backgroundIcon}`}
+          />
+          <span className={`${fontStyles.headFont} font-black!`}>
+            {"Scanning"}
+          </span>
+        </button>
+        <button
+          className={styles.fiftyButton}
+          onClick={() => {
+            setConfigurationType(true);
+          }}
+        >
+          <span
+            className={`icon-[material-symbols--print-rounded] ${styles.backgroundIcon}`}
+          />
+          <span className={`${fontStyles.headFont} font-black!`}>
+            {"Printing"}
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
